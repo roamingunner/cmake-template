74cba33da1712c0e07773ce25abf76a9f7b47466
leo <103922926@qq.com>
2021-01-21 10:54:48 +0000

    motify samples bug.

--- samples/network/basic_udp_client.c
+++ samples/network/basic_udp_client.c
@@ -12,6 +12,8 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <unistd.h>
+#define NETDB_MAX_HOST_NAME_LENGTH 128
 
 /**************************************************************************/
 /* Constants used by this program                                         */
@@ -27,121 +29,121 @@
 /* SERVER_NAME                             */
 void main(int argc, char *argv[])
 {
-   /***********************************************************************/
-   /* Variable and structure definitions.                                 */
-   /***********************************************************************/
-   int    sd, rc;
-   char   server[NETDB_MAX_HOST_NAME_LENGTH];
-   char   buffer[BUFFER_LENGTH];
-   struct sockaddr_in6 serveraddr;
-   int    serveraddrlen = sizeof(serveraddr);
-   struct addrinfo hints, *res;
+	/***********************************************************************/
+	/* Variable and structure definitions.                                 */
+	/***********************************************************************/
+	int    sd, rc;
+	char   server[NETDB_MAX_HOST_NAME_LENGTH];
+	char   buffer[BUFFER_LENGTH];
+	struct sockaddr_in6 serveraddr;
+	int    serveraddrlen = sizeof(serveraddr);
+	struct addrinfo hints, *res;
 
-   /***********************************************************************/
-   /* A do/while(FALSE) loop is used to make error cleanup easier.  The   */
-   /* close() of the socket descriptor is only done once at the very end  */
-   /* of the program.                                                     */
-   /***********************************************************************/
-   do
-   {
-      /********************************************************************/
-      /* The socket() function returns a socket descriptor, representing  */
-      /* an endpoint.  The statement also identifies that the INET6       */
-      /* (Internet Protocol) address family with the UDP transport        */
-      /* (SOCK_DGRAM) will be used for this socket.                       */
-      /********************************************************************/
-      sd = socket(AF_INET6, SOCK_DGRAM, 0);
-      if (sd < 0)
-      {
-         perror("socket() failed");
-         break;
-      }
+	/***********************************************************************/
+	/* A do/while(FALSE) loop is used to make error cleanup easier.  The   */
+	/* close() of the socket descriptor is only done once at the very end  */
+	/* of the program.                                                     */
+	/***********************************************************************/
+	do
+	{
+		/********************************************************************/
+		/* The socket() function returns a socket descriptor, representing  */
+		/* an endpoint.  The statement also identifies that the INET6       */
+		/* (Internet Protocol) address family with the UDP transport        */
+		/* (SOCK_DGRAM) will be used for this socket.                       */
+		/********************************************************************/
+		sd = socket(AF_INET6, SOCK_DGRAM, 0);
+		if (sd < 0)
+		{
+			perror("socket() failed");
+			break;
+		}
 
-      /********************************************************************/
-      /* If an argument was passed in, use this as the server, otherwise  */
-      /* use the #define that is located at the top of this program.      */
-      /********************************************************************/
-      if (argc > 1)
-         strcpy(server, argv[1]);
-      else
-         strcpy(server, SERVER_NAME);
+		/********************************************************************/
+		/* If an argument was passed in, use this as the server, otherwise  */
+		/* use the #define that is located at the top of this program.      */
+		/********************************************************************/
+		if (argc > 1)
+			strcpy(server, argv[1]);
+		else
+			strcpy(server, SERVER_NAME);
 
-      memset(&serveraddr, 0, sizeof(serveraddr));
-      serveraddr.sin6_family      = AF_INET6;
-      serveraddr.sin6_port        = htons(SERVER_PORT);
-      rc = inet_pton(AF_INET6, server, &serveraddr.sin6_addr.s6_addr);
-      if (rc != 1)
-      {
-         /*****************************************************************/
-         /* The server string that was passed into the inet_pton()        */
-         /* function was not a hexidecimal colon IP address.  It must     */
-         /* therefore be the hostname of the server.  Use the             */
-         /* getaddrinfo() function to retrieve the IP address of the      */
-         /* server.                                                       */
-         /*****************************************************************/
-         memset(&hints, 0, sizeof(hints));
-         hints.ai_family = AF_INET6;
-         hints.ai_flags = AI_V4MAPPED;
-         rc = getaddrinfo(server, NULL, &hints, &res);
-         if (rc != 0)
-         {
-            printf("Host not found! (%s)", server);
-            break;
-         }
+		memset(&serveraddr, 0, sizeof(serveraddr));
+		serveraddr.sin6_family      = AF_INET6;
+		serveraddr.sin6_port        = htons(SERVER_PORT);
+		rc = inet_pton(AF_INET6, server, &serveraddr.sin6_addr.s6_addr);
+		if (rc != 1)
+		{
+			/*****************************************************************/
+			/* The server string that was passed into the inet_pton()        */
+			/* function was not a hexidecimal colon IP address.  It must     */
+			/* therefore be the hostname of the server.  Use the             */
+			/* getaddrinfo() function to retrieve the IP address of the      */
+			/* server.                                                       */
+			/*****************************************************************/
+			memset(&hints, 0, sizeof(hints));
+			hints.ai_family = AF_INET6;
+			hints.ai_flags = AI_V4MAPPED;
+			rc = getaddrinfo(server, NULL, &hints, &res);
+			if (rc != 0)
+			{
+				printf("Host not found! (%s)", server);
+				break;
+			}
 
-         memcpy(&serveraddr.sin6_addr,
-                (&((struct sockaddr_in6 *)(res->ai_addr))->sin6_addr),
-                sizeof(serveraddr.sin6_addr));
+			memcpy(&serveraddr.sin6_addr,
+					(&((struct sockaddr_in6 *)(res->ai_addr))->sin6_addr),
+					sizeof(serveraddr.sin6_addr));
 
-         freeaddrinfo(res);
-      }
+			freeaddrinfo(res);
+		}
 
-      /********************************************************************/
-      /* Initialize the data block that is going to be sent to the server */
-      /********************************************************************/
-      memset(buffer, 0, sizeof(buffer));
-      strcpy(buffer, "A CLIENT REQUEST");
+		/********************************************************************/
+		/* Initialize the data block that is going to be sent to the server */
+		/********************************************************************/
+		memset(buffer, 0, sizeof(buffer));
+		strcpy(buffer, "A CLIENT REQUEST");
 
-      /********************************************************************/
-      /* Use the sendto() function to send the data to the server.        */
-      /********************************************************************/
-      rc = sendto(sd, buffer, sizeof(buffer), 0,
-                  (struct sockaddr *)&serveraddr,
-                  sizeof(serveraddr));
-      if (rc < 0)
-      {
-         perror("sendto() failed");
-         break;
-      }
+		/********************************************************************/
+		/* Use the sendto() function to send the data to the server.        */
+		/********************************************************************/
+		rc = sendto(sd, buffer, sizeof(buffer), 0,
+					(struct sockaddr *)&serveraddr,
+					sizeof(serveraddr));
+		if (rc < 0)
+		{
+			perror("sendto() failed");
+			break;
+		}
 
-      /********************************************************************/
-      /* Use the recvfrom() function to receive the data back from the    */
-      /* server.                                                          */
-      /********************************************************************/
-      rc = recvfrom(sd, buffer, sizeof(buffer), 0,
-                    (struct sockaddr *)&serveraddr,
-                    & serveraddrlen);
-      if (rc < 0)
-      {
-         perror("recvfrom() failed");
-         break;
-      }
+		/********************************************************************/
+		/* Use the recvfrom() function to receive the data back from the    */
+		/* server.                                                          */
+		/********************************************************************/
+		rc = recvfrom(sd, buffer, sizeof(buffer), 0,
+						(struct sockaddr *)&serveraddr,
+						& serveraddrlen);
+		if (rc < 0)
+		{
+			perror("recvfrom() failed");
+			break;
+		}
 
-      printf("client received the following: <%s>\n", buffer);
-      inet_ntop(AF_INET6, &serveraddr.sin6_addr.s6_addr,
-                buffer, sizeof(buffer));
-      printf("from port %d, from address %s\n",
-             ntohs(serveraddr_sin6_port), buffer);
+		printf("client received the following: <%s>\n", buffer);
+		inet_ntop(AF_INET6, &serveraddr.sin6_addr.s6_addr,
+					buffer, sizeof(buffer));
+		printf("from port %d, from address %s\n",
+				ntohs(serveraddr.sin6_port), buffer);
 
-      /********************************************************************/
-      /* Program complete                                                 */
-      /********************************************************************/
+		/********************************************************************/
+		/* Program complete                                                 */
+		/********************************************************************/
 
-   } while (FALSE);
+	} while (FALSE);
 
-   /***********************************************************************/
-   /* Close down any open socket descriptors                              */
-   /***********************************************************************/
-   if (sd != -1)
-      close(sd);
+	/***********************************************************************/
+	/* Close down any open socket descriptors                              */
+	/***********************************************************************/
+	if (sd != -1)
+		close(sd);
 }
--- samples/network/basic_udp_server.c
+++ samples/network/basic_udp_server.c
@@ -10,6 +10,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
 
 /**************************************************************************/
 /* Constants used by this program                                         */
@@ -20,94 +22,94 @@
 
 void main()
 {
-   /***********************************************************************/
-   /* Variable and structure definitions.                                 */
-   /***********************************************************************/
-   int    sd=-1, rc;
-   char   buffer[BUFFER_LENGTH];
-   struct sockaddr_in6 serveraddr;
-   struct sockaddr_in6 clientaddr;
-   int    clientaddrlen = sizeof(clientaddr);
+/***********************************************************************/
+/* Variable and structure definitions.                                 */
+/***********************************************************************/
+int    sd=-1, rc;
+char   buffer[BUFFER_LENGTH];
+struct sockaddr_in6 serveraddr;
+struct sockaddr_in6 clientaddr;
+int    clientaddrlen = sizeof(clientaddr);
 
-   /***********************************************************************/
-   /* A do/while(FALSE) loop is used to make error cleanup easier.  The   */
-   /* close() of each of the socket descriptors is only done once at the  */
-   /* very end of the program.                                            */
-   /***********************************************************************/
-   do
-   {
-      /********************************************************************/
-      /* The socket() function returns a socket descriptor, representing  */
-      /* an endpoint.  The statement also identifies that the INET6       */
-      /* (Internet Protocol version 6) address family with the UDP        */
-      /* transport (SOCK_DGRAM) will be used for this socket.             */
-      /********************************************************************/
-      sd = socket(AF_INET6, SOCK_DGRAM, 0);
-      if (sd < 0)
-      {
-         perror("socket() failed");
-         break;
-      }
+/***********************************************************************/
+/* A do/while(FALSE) loop is used to make error cleanup easier.  The   */
+/* close() of each of the socket descriptors is only done once at the  */
+/* very end of the program.                                            */
+/***********************************************************************/
+do
+{
+	/********************************************************************/
+	/* The socket() function returns a socket descriptor, representing  */
+	/* an endpoint.  The statement also identifies that the INET6       */
+	/* (Internet Protocol version 6) address family with the UDP        */
+	/* transport (SOCK_DGRAM) will be used for this socket.             */
+	/********************************************************************/
+	sd = socket(AF_INET6, SOCK_DGRAM, 0);
+	if (sd < 0)
+	{
+		perror("socket() failed");
+		break;
+	}
 
-      /********************************************************************/
-      /* After the socket descriptor is created, a bind() function gets a */
-      /* unique name for the socket.  In this example, the user sets the  */
-      /* s_addr to zero, which means that the UDP port of 3555 will be    */
-      /* bound to all IP addresses on the system.                         */
-      /********************************************************************/
-      memset(&serveraddr, 0, sizeof(serveraddr));
-      serveraddr.sin6_family      = AF_INET6;
-      serveraddr.sin6_port        = htons(SERVER_PORT);
-      memcpy(&serveraddr.sin6_addr, &in6addr_any, sizeof(in6addr_any));
+	/********************************************************************/
+	/* After the socket descriptor is created, a bind() function gets a */
+	/* unique name for the socket.  In this example, the user sets the  */
+	/* s_addr to zero, which means that the UDP port of 3555 will be    */
+	/* bound to all IP addresses on the system.                         */
+	/********************************************************************/
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin6_family      = AF_INET6;
+	serveraddr.sin6_port        = htons(SERVER_PORT);
+	memcpy(&serveraddr.sin6_addr, &in6addr_any, sizeof(in6addr_any));
 
-      rc = bind(sd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
-      if (rc < 0)
-      {
-         perror("bind() failed");
-         break;
-      }
+	rc = bind(sd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+	if (rc < 0)
+	{
+		perror("bind() failed");
+		break;
+	}
 
-      /********************************************************************/
-      /* The server uses the recvfrom() function to receive that data.    */
-      /* The recvfrom() function waits indefinitely for data to arrive.   */
-      /********************************************************************/
-      rc = recvfrom(sd, buffer, sizeof(buffer), 0,
-                    (struct sockaddr *)&clientaddr,
-                    &clientaddrlen);
-      if (rc < 0)
-      {
-         perror("recvfrom() failed");
-         break;
-      }
+	/********************************************************************/
+	/* The server uses the recvfrom() function to receive that data.    */
+	/* The recvfrom() function waits indefinitely for data to arrive.   */
+	/********************************************************************/
+	rc = recvfrom(sd, buffer, sizeof(buffer), 0,
+					(struct sockaddr *)&clientaddr,
+					&clientaddrlen);
+	if (rc < 0)
+	{
+		perror("recvfrom() failed");
+		break;
+	}
 
-      printf("server received the following: <%s>\n", buffer);
-      inet_ntop(AF_INET6, &clientaddr.sin6_addr.s6_addr,
-                buffer, sizeof(buffer));
-      printf("from port %d and address %s\n",
-             ntohs(clientaddr.sin6_port),
-             buffer);
+	printf("server received the following: <%s>\n", buffer);
+	inet_ntop(AF_INET6, &clientaddr.sin6_addr.s6_addr,
+				buffer, sizeof(buffer));
+	printf("from port %d and address %s\n",
+			ntohs(clientaddr.sin6_port),
+			buffer);
 
-      /********************************************************************/
-      /* Echo the data back to the client                                 */
-      /********************************************************************/
-      rc = sendto(sd, buffer, sizeof(buffer), 0,
-                  (struct sockaddr *)&clientaddr,
-                  sizeof(clientaddr));
-      if (rc < 0)
-      {
-         perror("sendto() failed");
-         break;
-      }
+	/********************************************************************/
+	/* Echo the data back to the client                                 */
+	/********************************************************************/
+	rc = sendto(sd, buffer, sizeof(buffer), 0,
+				(struct sockaddr *)&clientaddr,
+				sizeof(clientaddr));
+	if (rc < 0)
+	{
+		perror("sendto() failed");
+		break;
+	}
 
-      /********************************************************************/
-      /* Program complete                                                 */
-      /********************************************************************/
+	/********************************************************************/
+	/* Program complete                                                 */
+	/********************************************************************/
 
-   } while (FALSE);
+} while (FALSE);
 
-   /***********************************************************************/
-   /* Close down any open socket descriptors                              */
-   /***********************************************************************/
-   if (sd != -1)
-      close(sd);
+/***********************************************************************/
+/* Close down any open socket descriptors                              */
+/***********************************************************************/
+if (sd != -1)
+	close(sd);
 }
