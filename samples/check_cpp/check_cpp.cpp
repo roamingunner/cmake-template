841ad795608565bf13831ebdd49ce2219c93c399
Leo <103922926@qq.com>
2021-02-02 09:37:39 +0800

    Update check_cpp.cpp
    show c++ version during buildtime

--- samples/check_cpp/check_cpp.cpp
+++ samples/check_cpp/check_cpp.cpp
@@ -2,14 +2,19 @@
 
 int main(){
         #if __cplusplus==201500L
+#warning support C++17
         std::cout << "C++17" << std::endl;
         #elif __cplusplus==201402L
+#warning support C++14
         std::cout << "C++14" << std::endl;
         #elif __cplusplus==201103L
+#warning support C++11
         std::cout << "C++11" << std::endl;
         #elif __cplusplus==199711L
+#warning support C++98
         std::cout << "C++98" << std::endl;
         #else
+#warning support C++
         std::cout << "C++" << std::endl;
         #endif
 
